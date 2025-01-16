e2d0b48f39245506dd74fa08074275132357dddd
JoshLikesToCode <jstone925314@gmail.com>
2025-01-15 17:50:01 -0800

    Transitioned minimal APIs into controllers

--- TheEmployeeAPI/BaseController.cs
+++ TheEmployeeAPI/BaseController.cs
@@ -1,8 +1,23 @@
+using FluentValidation.Results;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TheEmployeeAPI;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("[controller]")]
 public abstract class BaseController : Controller
-{}
+{
+        protected async Task<ValidationResult> ValidateAsync<T>(T instance)
+    {
+        var validator = HttpContext.RequestServices.GetService<IValidator<T>>();
+        if (validator == null)
+        {
+            throw new ArgumentException($"No validator found for {typeof(T).Name}");
+        }
+        var validationContext = new ValidationContext<T>(instance);
+
+        return await validator.ValidateAsync(validationContext);
+    }   
+
+}
